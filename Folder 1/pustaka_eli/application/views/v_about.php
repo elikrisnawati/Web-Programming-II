0eb555c062de7b112bbfcadaec8c43c617fd1b59
elikrisnawati <elikrisnawati13@gmail.com>
2020-10-26 17:45:55 +0700

    tugas

---
+++ Folder 1/pustaka_eli/application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Eli</li>
+        <li>Nama Belakang : Krisnawati</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Pagejugan</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Brebes</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>jalan santai</li>
+    </ul>
+</section>
---
+++ Folder 2/Praktikum 1/perkenalan.php
@@ -0,0 +1,10 @@
+<?php
+class perkenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Eli Krisnawati<br>
+                saya tinggal di Brebes<br>
+                saya ingin sehat dan kaya";
+    }
+
+}
